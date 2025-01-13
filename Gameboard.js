05e1f327171511e0742e7fd2647885c8fdcec494
benjaminfrias <benjaminfrias2004@gmail.com>
2025-01-13 15:08:42 -0400

    add receive attack method

--- Gameboard.js
+++ Gameboard.js
@@ -1,6 +1,8 @@
 export class Gameboard {
 	constructor() {
 		this.board = new Map();
+		this.prevShoots = new Map();
+		this.ships = [];
 	}
 
 	placeShip(ship, shipCoords) {
@@ -9,18 +11,41 @@ export class Gameboard {
 			const singleCoorString = singleCoordinate.toString();
 
 			if (!this.board.has(singleCoorString)) {
-				this.board.set(singleCoorString, {
-					ship: ship,
-					isAttacked: false,
-				});
+				this.board.set(singleCoorString, ship);
+				this.ships.push(ship);
 			} else {
 				throw Error("This coordinate has a ship already");
 			}
 		}
 	}
 
+	receiveAttack(coordinates) {
+		const coordString = coordinates.toString();
+
+		// If coordinates exist in previous shoots, return
+		if (this.prevShoots.has(coordString)) {
+			return "already shooted";
+		}
+
+		// If coordinates exist in board, hit ship
+		if (this.board.has(coordString)) {
+			const ship = this.board.get(coordString);
+			ship.hit();
+		}
+
+		this.prevShoots.set(coordString, coordinates);
+	}
+
+	isGameOver() {
+		// TODO: Check if all player's ships has been sunk
+	}
+
+	getPreviousShoots() {
+		return Array.from(this.prevShoots.values());
+	}
+
 	getCoordinates(coordinates) {
 		const string = coordinates.toString();
-		return this.board.get(string);
+		return this.board.get(string) || null;
 	}
 }
--- battleship.test.js
+++ battleship.test.js
@@ -18,7 +18,20 @@ it("Gameboard: Placing a ship in specific coordinates", () => {
 	gameboard.placeShip(ship, coordinates);
 
 	expect(gameboard.getCoordinates(coordinates[0])).toEqual({
-		isAttacked: false,
-		ship: { hits: 0, isDestroyed: false, length: 2 },
+		hits: 0,
+		isDestroyed: false,
+		length: 2,
 	});
+});
+it("Gameboard: Get previous shoots", () => {
+	const gameboard = new Gameboard();
+	const ship = new Ship(2);
+	const coordinates = [
+		[1, 2],
+		[1, 3],
+	];
+	gameboard.placeShip(ship, coordinates);
+	gameboard.receiveAttack([1, 2], ship);
+
+	expect(gameboard.getPreviousShoots()).toEqual([[1, 2]]);
 });
