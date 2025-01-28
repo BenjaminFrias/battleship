1a4a1bf21db954b7e0c17cdaeec95779e9f186e3
benjaminfrias <benjaminfrias2004@gmail.com>
2025-01-28 16:04:28 -0400

    refactor: create gameboards elements in method

--- DOMHandler.js
+++ DOMHandler.js
@@ -1,12 +1,22 @@
 export class DOMHandler {
 	constructor() {
 		this.GRIDSIZE = 10;
-		this.playerGameboard = document.querySelector("#player-gameboard");
-		this.opponentGameboard = document.querySelector("#opponent-gameboard");
+		this.gameboardContainer = document.querySelector("#gameboards");
 		this.cells = [];
 	}
 
 	createGameboards() {
+		// Create players' gameboards
+		this.playerGameboard = document.createElement("div");
+		this.playerGameboard.id = "player-gameboard";
+
+		this.opponentGameboard = document.createElement("div");
+		this.opponentGameboard.id = "opponent-gameboard";
+
+		this.gameboardContainer.appendChild(this.playerGameboard);
+		this.gameboardContainer.appendChild(this.opponentGameboard);
+
+		// Create cells
 		for (let row = 0; row < this.GRIDSIZE; row++) {
 			for (let col = 0; col < this.GRIDSIZE; col++) {
 				// Create single board cell
