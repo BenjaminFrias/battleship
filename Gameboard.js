b988f398949f9e4df40665f11f2bd3d889234793
benjaminfrias <benjaminfrias2004@gmail.com>
2025-02-15 17:29:21 -0400

    Feature(Coords validation): check for coordinates contiguity

--- Gameboard.js
+++ Gameboard.js
@@ -92,11 +92,22 @@ export class Gameboard {
 
 			// Check if coords already exist in board
 			const transformedCoords = this.transformCoordinates(
-				splittedCoords.join("")
+				splittedCoords.join(",")
 			);
+
 			const isCellOccupied = this.checkExistingCoords(transformedCoords);
 
 			if (isCellOccupied) {
+				return false;
+			}
+
+			// TODO: validate contiguous ship coordinates
+			// Check for contiguous coordinates
+
+			const contiguityResult =
+				this.checkContiguousCoords(transformedCoords);
+
+			if (!contiguityResult) {
 				return false;
 			}
 
@@ -164,6 +175,46 @@ export class Gameboard {
 			const y = parseInt(coordinate.slice(1)) - 1;
 
 			return [x, y];
+		}
+	}
+
+	checkContiguousCoords(coords) {
+		let orientation;
+
+		// Check orientation
+		let x = coords[0][0];
+		let y = coords[0][1];
+
+		if (x + 1 == coords[1][0] && y == coords[1][1]) {
+			orientation = "horizontal";
+		} else if (y + 1 == coords[1][1] && x == coords[1][0]) {
+			orientation = "vertical";
+		} else {
+			return false;
+		}
+
+		if (orientation == "horizontal") {
+			// Check for horizontal contiguity
+			for (let i = 0; i < coords.length - 1; i++) {
+				let x = coords[i][0];
+				let y = coords[i][1];
+
+				if (x + 1 != coords[i + 1][0] || y != coords[i + 1][1]) {
+					return false;
+				}
+			}
+			return true;
+		} else if (orientation == "vertical") {
+			// Check for vertical contiguity
+			for (let i = 0; i < coords.length - 1; i++) {
+				let x = coords[i][0];
+				let y = coords[i][1];
+
+				if (y + 1 != coords[i + 1][1] || x != coords[i + 1][0]) {
+					return false;
+				}
+			}
+			return true;
 		}
 	}
 
